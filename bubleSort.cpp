757b4a85770bcfbf1cca73c6e965a46d1bfc20bd
Sánchez <atsanchez@FRSN.UTN.EDU.AR>
2024-10-15 15:37:49 -0300

    metodo burbuja y arrays

---
+++ bubleSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int largoDelArray = 5;
+    int array[largoDelArray];
+    ///Asi ingresamos elemnetos a un array
+    cout << "Ingrese los numeros del array " << endl;
+    for(int i = 0; i < largoDelArray; i++){
+        cin >> array[i];
+    }
+
+    cout << endl;
+
+    ///mostramos el array
+    cout << "Mostrando array: " << endl;
+    for(int i = 0; i < largoDelArray; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    ///algoritmo para el metodo de burbuja
+    for(int i = 0; i < largoDelArray - 1; i++){
+        for(int j = 0; j < largoDelArray - i - 1; j++){
+            if(array[j] > array[j + 1]){
+                ///aca van a pasar los intercambios
+                int aux = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = aux;
+            }
+        }
+    }
+
+    cout << "Numeros ordenados: " << endl;
+
+    for(int i = 0; i < largoDelArray; i++){
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
--- matrizArrayUnidimensional.cpp
+++ matrizArrayUnidimensional.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 int main(){
 
+    int matriz[3][3], arrayUnidimensional[9];
+    int k = 0; ///indice para el arrayUnidimensional
 
+    ///armar la matriz
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            cout << "Elementos [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
+
+    ///convertir la matriz en un array unido
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+                arrayUnidimensional[k++] = matriz[i][j];
+        }
+    }
+
+    ///mostrar el array
+    cout << "Array resultado: " << endl;
+    for(int i = 0; i < 9; i++){
+        cout << arrayUnidimensional[i] << " ";
+    }
 
     return 0;
 }
